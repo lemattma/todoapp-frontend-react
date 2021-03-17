8d725ead4584d28e433301a12f831e8f74caa6a9
Martin Miranda <martinmirandac@gmail.com>
2021-03-17 14:56:02 +1100

    added new list functionality

--- src/components/lists.js
+++ src/components/lists.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 
 class Lists extends Component {
-    state = { lists: [] }
+    state = {
+        lists: []
+    }
 
     componentDidMount() {
+        this.loadLists()
+    }
+
+    newListEnter(event) {
+        if (event.code === 'Enter' && event.target.value !== '') {
+            this.saveNewList(event.target.value);
+        }
+    }
+
+    saveNewList(name) {
+        fetch('http://localhost:8080/lists', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({ name: name })
+        })
+        .then(() => this.loadLists())
+        .catch(console.log)
+    }
+
+    loadLists() {
         fetch('http://localhost:8080/lists')
             .then(res => res.json())
             .then((data) => {
-                this.setState({ lists: data })
+                this.listName.value = '';
+                this.setState({ lists: data });
             })
             .catch(console.log)
     }
@@ -22,6 +45,14 @@ class Lists extends Component {
                         <Link to={`/lists/${list.id}`} class="list-group-item list-group-item-action">{list.name}</Link>
                     ))}
                 </div>
+
+                <input
+                    type="email"
+                    class="form-control form-control-lg mt-5"
+                    onKeyPress={this.newListEnter.bind(this)}
+                    placeholder="Enter new list"
+                    ref={(el) => (this.listName = el)}
+                />
             </div>
         )
     }
